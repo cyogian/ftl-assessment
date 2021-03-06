2d9dd4250883b115b31d54389f275b3c90a5eda9
Amar Nath Yogi <contact@cyogian.dev>
2021-03-06 06:12:19 +0530

    completed member search feature

--- src/containers/main.js
+++ src/containers/main.js
@@ -4,7 +4,8 @@ import {
     Header,
     List,
     Segment,
-    Icon
+    Icon,
+    Input
 } from 'semantic-ui-react'
 
 import axios from "../utilities/axios"
@@ -19,7 +20,34 @@ class Main extends Component {
         currentMemberID: "",
         currentMemberTZ: "",
         currentMemberNAME: "",
-        isLoading: true
+        isLoading: true,
+        searchTerm: "",
+        searchLoading: false
+    }
+
+    onHandleSearch = (e) => {
+        let value = e.target.value
+        let searchLoading = false
+        if (value !== "") {
+            searchLoading = true
+            let url = `/members`
+            let options = {
+                params: {
+                    q: value
+                }
+            }
+            axios.get(url, options)
+                .then((response) => {
+                    this.setState({
+                        searchLoading: false,
+                        members: response.data
+                    })
+                })
+        }
+        this.setState({
+            searchTerm: value,
+            searchLoading: searchLoading
+        })
     }
 
     onOpen = (id, tz, name) => {
@@ -73,7 +101,8 @@ class Main extends Component {
                 <h1 className="alternate-heading">Member Activity Viewer</h1>
                 <Header as='h2' style={{ textAlign: "center", color: "brown", borderBottom: "3px solid black", padding: "0.5rem 0rem", marginTop: "0", backgroundColor: "rgb(250,250,250)", boxShadow: "0 0 2px 1px black" }}><Icon size="mini" name="list alternate outline" />Members</Header>
                 <Segment loading={this.state.isLoading} style={{ margin: "0.5rem auto", maxWidth: "850px" }}>
-                    {members.length ? <List celled>{members}</List> : "No members to display in this list"}
+                    <Input icon="search" loading={this.state.searchLoading} type="text" value={this.state.searchTerm} placeholder="Search Members..." onChange={this.onHandleSearch} />
+                    {members.length ? <List celled>{members}</List> : <span style={{ padding: "0.4rem", marginLeft: "1rem", color: "crimson", border: "2px solid #CF000F", borderRadius: "4px" }}>"No members to display in this list"</span>}
                 </Segment>
                 <ActivityPeriods open={isOpen} onClose={this.onClose} currentMemberNAME={currentMemberNAME} currentMemberID={currentMemberID} currentMemberTZ={currentMemberTZ} />
             </Container >
